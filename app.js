3048ee754d58e49254d6074eb26a27622c31076d
Washington <221032910@logon.ds.ge.com>
2018-10-21 11:45:57 -0400

    update

--- app.js
+++ app.js
@@ -9,3 +9,44 @@ alert(patientList);
 
 const command = prompt('Choose one: update, delete, add, reorder');
 
+if (command ==='add') {
+  const add = prompt('Patient Name?');
+  patientList.push(add);
+  alert(patientList);
+}
+
+
+else if (command ==='update') {
+  const update = prompt ('Patient Name');
+  const newName = prompt ('Patient New Name?');
+  for (let i=0; patientList.length; i++) {
+    if (patientList[i]=== update){
+      patientList[i] = newName;
+    alert(patientList);
+    }
+  }
+}
+
+else if (command ==='delete') {
+  const add = prompt('Patient Name');
+  for (let i=0; patientList.length; i++) {
+    if (patientList[i]=== add){
+    patientList.splice(i,1);
+   alert(patientList); 
+    }
+  }
+}
+
+if (command ==='reorder') {
+  const add = prompt('Patient Name');
+  const anotherName = prompt('Patient Name');
+  for (let i=0; patientList.length; i++) {
+    if(patientList[i]=== add){
+      patientList.splice(i,1,anotherName);    
+    }
+    if(patientList[i]=== anotherName){
+      patientList.splice(i,1,add); 
+    }
+  alert(patientList);  
+  }  
+}
